e796531d70644b2956157b75e0c255a7b7004296
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-07-31 22:45:48 -0300

    organizando projeto

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import GlobalStyle from "../../styles/GlobalStyle"
+import Header from "../Header/Header"
+
+const App = () => {
+    return(
+        <>
+            <GlobalStyle/>
+            <Header/>
+            <main>
+                <p>testando estilos</p>
+            </main>
+        </>
+    )
+}
+
+
+export default App
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0px;
+        background-color: black;
+        color: white;
+    }
+
+    main{
+        font-family: Arial, Helvetica, sans-serif;
+    }
+`
+
+export default GlobalStyle
