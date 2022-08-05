6c110594a902a87d46094dea62472390a57bba8b
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-08-04 21:50:39 -0300

    adicionando barra de rolagem dos repositórios

--- src/components/Repos/Repos.js
+++ src/components/Repos/Repos.js
@@ -9,11 +9,32 @@ const Repos = () => {
     return(
         <Wrapper>
             <Repository NumberRepo={0}/>
+            <Repository NumberRepo={1}/>
+            <Repository NumberRepo={2}/>
+            <Repository NumberRepo={3}/>
+            <Repository NumberRepo={4}/>
+            <Repository NumberRepo={5}/>
+            <Repository NumberRepo={6}/>
+            <Repository NumberRepo={7}/>
+            <Repository NumberRepo={8}/>
+            <Repository NumberRepo={9}/>
+            <Repository NumberRepo={10}/>
+            <Repository NumberRepo={11}/>
+            <Repository NumberRepo={12}/>
         </Wrapper>
     )
 }
 const Wrapper = styled.div`
     width: 50%;
     height: 100%;
+    overflow-y: scroll;
+    ::-webkit-scrollbar{
+        width: 8px;
+        background-color: #222;
+    }
+    ::-webkit-scrollbar-thumb{
+        background-color: green;
+        border-radius: 8px;
+    }
 `
 export default Repos;
